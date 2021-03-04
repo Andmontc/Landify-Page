e16e65ba85ba606e636c82a0d2138f59fc461cc1
Andmontc <oscarandresmontes@gmail.com>
2021-03-04 16:26:32 -0500

    Update: section logo component

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const $menu = document.querySelector('.js-header-menu');
 const $head = document.querySelector('.js-header');
 const $landing = document.querySelector('.js-landing');
+const $logo = document.querySelector('.js-logos');
 const sticky = $head.offsetTop;
 
 window.onscroll = handleStickyScroll
@@ -8,7 +9,8 @@ window.onscroll = handleStickyScroll
 $menu.addEventListener('click', () => {
   const header = $head.querySelector('.header__container');
   header.classList.toggle('header__active');
-  $landing.classList.toggle('landing__hidden');
+  $landing.classList.toggle('hidden');
+  $logo.classList.toggle('hidden');
 })
 
 function handleStickyScroll () {
